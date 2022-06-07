4b00e47b57a7799c53612bccf060a1f9e619636d
Andre <andre.christian.silva@gmail.com>
2022-06-06 22:02:15 -0700

    Finished

--- practice-for-week-08-create-and-handle-promises-main/practice-for-week-08-create-and-handle-promises-main/phase-1.js
+++ practice-for-week-08-create-and-handle-promises-main/practice-for-week-08-create-and-handle-promises-main/phase-1.js
@@ -1,17 +1,53 @@
 function stretch() {
-  // Your code here
+  // create a new promise
+  // fulfill the promise after 1 second using setTimeOut()
+  // print done stretch when promise is done
+  const newPromise = new Promise((res, rej) => {
+    setTimeout(() => {
+      res("done stretching")
+    }, 1000)
+  });
+
+  return newPromise
 }
 
 function runOnTreadmill() {
-  // Your code here
+  const newPromise = new Promise((res, rej) => {
+    setTimeout(() => {
+      res("done running on treadmill")
+    }, 500)
+  });
+  return newPromise
 }
 
 function liftWeights() {
-  // Your code here
+  const newPromise = new Promise((res, rej) => {
+    setTimeout(() => {
+      res("done lifting weights")
+    }, 2000)
+
+
+  });
+  return newPromise
+
 }
 
 function workout() {
-  // Your code here
+  stretch().then((res) => {
+    console.log(res)
+    return runOnTreadmill()
+  })
+    .then((res) => {
+      console.log(res)
+      return liftWeights()
+    })
+    .then((res) => {
+      console.log(res)
+      console.log("done working out")
+    })
+
+  // liftWeights().then((res) => console.log(res))
+
 }
 
 
@@ -20,6 +56,7 @@ function workout() {
 Run the file (`node phase-1.js`) and check your output against the expected
 output.
 */
+
 
 
 workout();
